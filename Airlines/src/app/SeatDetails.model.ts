70d2111e92f6e355b3d092c61817635c85c7e0d0
Mayankkkyadav <11997mayank@gmail.com>
2020-10-07 12:27:41 +0530

    M

---
+++ Airlines/src/app/SeatDetails.model.ts
@@ -0,0 +1,4 @@
+export class SeatDetails{
+    flightId : number;
+    dateOfJourney : string;
+}
--- Airlines/src/app/seat-select/seat-select.component.ts
+++ Airlines/src/app/seat-select/seat-select.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {FlightService} from '../services/flight.service'
+import { SeatDetails } from '../SeatDetails.model';
 
 @Component({
   selector: 'app-seat-select',
@@ -6,10 +9,88 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seat-select.component.css']
 })
 export class SeatSelectComponent implements OnInit {
+  tempid: any;
+  fare: number = 0;
+  totalFare: number = 0;
 
-  constructor() { }
+  constructor(private flightservice: FlightService, private router: Router) { }
+  selected: boolean = false;
+  selectSeats = new Array<number>();
+  selectedSeatNumber: string;
+  seatdetails: SeatDetails = new SeatDetails();
+  limitExceeds: string = "Cannot book more than 3 Seats."
+  limit: boolean = false;
+
+  
+  selectseat(idin: any) {
+    this.tempid = idin;
+    var s;
+    var id = document.getElementById(idin)
+
+    var result = this.findIndex(idin);
+    if (result >= 0) {
+      this.limit = false;
+      this.selectSeats.splice(result, 1);
+      document.getElementById(idin).style.backgroundColor = "rgb(211, 183, 183)";
+      // var x = document.getElementById(idin);
+      // x.setAttribute("src", "../assets/bookseat.png");
+      if (this.selectSeats.length == 0) {
+        this.fare = 0;
+        this.totalFare = this.fare;
+      }
+      else {
+        this.fare = this.fare - Number(sessionStorage.getItem('fare'));
+        this.totalFare = this.fare;
+      }
+    }
+    else {
+       ("else callled");
+
+      if (this.selectSeats.length == 0) {
+        this.fare = Number(sessionStorage.getItem('fare'));
+        this.totalFare = this.fare;
+      }
+      if (this.selectSeats.length <= 2) {
+
+        this.selectSeats.push(idin);
+        this.myFunction(idin);
+        this.fare = Number(sessionStorage.getItem('fare')) * this.selectSeats.length;
+        this.totalFare = this.fare;
+      }
+      else {
+        this.limit = true;
+      }
+    }
+    // this.selectSeats.sort((a,b)=>a-b)
+    // this.selectedSeatNumber = String(this.selectSeats.sort().toString().split(","));
+  
+    this.selectedSeatNumber = (String(this.selectSeats.sort((a,b)=>a-b)));
+  }
+  myFunction(idin) {
+    (idin + " inside func");
+   document.getElementById(idin).style.backgroundColor = "yellow";
+ }
+ findIndex(id1) {
+
+   for (let i = 0; i < this.selectSeats.length; i++) {
+
+     if (id1 == this.selectSeats[i]) {
+       return i;
+     }
+
+   }
+   return -1;
+ }
+ 
+  storeSeatDetails() {
+    sessionStorage.setItem("seatsBooked", JSON.stringify(this.selectSeats));
+    sessionStorage.setItem("totalFare", String(this.totalFare));
+    this.router.navigate(['bookTicket']);
+  }
+
 
   ngOnInit(): void {
+    
   }
 
 }
